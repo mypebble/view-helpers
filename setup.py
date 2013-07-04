56eeec93f3aa5835ac4c8cc5586cd56d7f708fa7
scw <devnull@localhost>
2013-07-04 12:26:42 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pebble-view-helpers',
+    version='0.0.1',
+    description="",
+    author="SF Software limited t/a Pebble",
+    author_email="sdev@talktopebble.co.uk",
+    url="https://www.mypebble.co.uk",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
