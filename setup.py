0d7d67c05a2bda7bb09caa0febf323bb045fe078
scw <devnull@localhost>
2013-09-02 18:05:08 +0100

    Bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pebble-view-helpers',
-    version='0.0.5',
+    version='0.0.6',
     description="Set of helpers to make working with, and testing, generic views easier",
     author="SF Software limited t/a Pebble",
     author_email="sdev@talktopebble.co.uk",
