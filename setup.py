cd5fa3fa952d246682298fd9a6019953cd25671c
Scott Walton <scott.walton@mypebble.co.uk>
2018-02-06 15:55:05 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,30 @@
+"""Setup config.
+"""
+from os import path
 from setuptools import setup, find_packages
+
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.5',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Framework :: Django',
+]
+
+
+README_FILE = path.join(path.dirname(path.abspath(__file__)), 'README.md')
+
+try:
+    import m2r
+    LONG_DESCRIPTION = m2r.parse_from_file(README_FILE)
+except Exception:
+    LONG_DESCRIPTION = ''
+
 
 setup(
     name='pebble-view-helpers',
@@ -14,4 +40,6 @@ setup(
         'python-dateutil>=1.5',
         'django>=1.11',
     ],
+    long_description=LONG_DESCRIPTION,
+    classifiers=CLASSIFIERS,
 )
